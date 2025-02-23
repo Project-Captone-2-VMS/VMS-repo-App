80e24570cd21f1b2bae2d66e6597450d91068cc8
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-02-23 23:33:10 +0700

    chore: initial project setup with MVVM structure

---
+++ vms_app/lib/api/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static const String baseApiUrl = 'http://10.0.2.2:8080';
+}
---
+++ vms_app/lib/core/constants/app_routers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/onboarding1',
+    routes: [],
+  );
+}
