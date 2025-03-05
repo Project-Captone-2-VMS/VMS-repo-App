87fee753d9d98b835e9e1ef21a10d4f108b45bc2
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-03-05 18:20:48 +0700

    chore: add theme app

---
+++ vms_app/lib/core/theme/app_text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const heading = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+  static const appbarText = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static const subheading = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+  static const description = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const body = TextStyle(fontSize: 16, color: Colors.white70);
+}
--- vms_app/lib/core/theme/my_theme.dart
+++ vms_app/lib/core/theme/my_theme.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class MyTheme {
+  static var primaryColor = Color(0xFFF9742A);
+  static var white = Colors.white;
+  static var grey = Colors.grey;
+  static var black = Colors.black;
+  static var customYellowWithOrangeShade = Color(0xFFF59762);
+  static var customGreenColor = Color(0xFF29D697);
+  static var customBlueColor = Color.fromARGB(255, 41, 142, 214);
+  static const bottomBarBgColor = Color.fromARGB(255, 16, 20, 27);
+  static var backgroundcolor = Color(0xFF1A1F28);
+}
