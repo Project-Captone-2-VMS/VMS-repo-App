f9a53760fd444ee0453f352e473781e7c22750d5
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-03-06 23:03:28 +0700

    feat: add constants for map

---
+++ vms_app/lib/core/utils/map_constants.dart
@@ -0,0 +1,5 @@
+import 'package:latlong2/latlong.dart';
+
+const LatLng initialLocation = LatLng(21.0285, 105.8542);
+const Duration simulationInterval = Duration(milliseconds: 50000);
+const double simulationDistance = 0.0001;
