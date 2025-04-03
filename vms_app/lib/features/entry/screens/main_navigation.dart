c3d6ed6472ec9fdac1cb8018afd5edcc36a280de
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-04-04 00:11:11 +0700

    feat: add wrap screen

---
+++ vms_app/lib/features/entry/screens/main_navigation.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:vms_app/features/entry/widgets/bottom_navigation_widget.dart';
+import 'package:vms_app/features/home/presentation/ui/screens/home_screen.dart';
+import 'package:vms_app/features/wallet/presentation/ui/wallet_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  // Danh sách các screen
+  final List<Widget> _screens = [
+    TruckerHomeScreen(),
+    WalletScreen(),
+    // MapScreen(),
+    // ProfileScreen(),
+  ];
+
+  void _onItemSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationWidget(
+        selectedIndex: _selectedIndex,
+        onItemSelected: _onItemSelected,
+      ),
+    );
+  }
+}
