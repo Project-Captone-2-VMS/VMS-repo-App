8133ff1673d64169d1c96511c015efb6c10b5367
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-04-04 00:13:10 +0700

    feat: add splash screen

---
+++ vms_app/lib/features/entry/screens/splash_screen.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vms_app/config/theme/app_theme.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat();
+    Timer(const Duration(milliseconds: 1500), () {
+      context.go('/');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (_, child) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: CustomPaint(
+                        painter: CircleProgressPainter(
+                          animation: _controller,
+                          color: AppTheme.primaryColor,
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            242,
+                            242,
+                            242,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(color: Colors.transparent),
+                      child: Image(
+                        image: AssetImage('assets/images/logo_truck.png'),
+                      ),
+                    ),
+                    const Positioned(
+                      bottom: 25,
+                      child: Text(
+                        'VMS',
+                        style: TextStyle(
+                          color: AppTheme.primaryColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CircleProgressPainter extends CustomPainter {
+  final Animation<double> animation;
+  final Color color;
+  final Color backgroundColor;
+
+  CircleProgressPainter({
+    required this.animation,
+    required this.color,
+    required this.backgroundColor,
+  }) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = math.min(size.width, size.height) / 2;
+    final backgroundPaint =
+        Paint()
+          ..color = backgroundColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 15.0;
+
+    canvas.drawCircle(center, radius, backgroundPaint);
+    final progressPaint =
+        Paint()
+          ..color = color
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 15.0
+          ..strokeCap = StrokeCap.round;
+
+    final progressAngle = 2 * math.pi * animation.value;
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -math.pi / 2,
+      progressAngle,
+      false,
+      progressPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CircleProgressPainter oldDelegate) {
+    return animation != oldDelegate.animation ||
+        color != oldDelegate.color ||
+        backgroundColor != oldDelegate.backgroundColor;
+  }
+}
