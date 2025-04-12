a1fbe0bdd2b23bf7aa0a63c4b0a473198d33b30d
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-04-12 20:53:26 +0700

    chore: update route

--- vms_app/lib/config/routes/router.dart
+++ vms_app/lib/config/routes/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:vms_app/features/Alert/presentation/ui/screens/detail_problem_screen.dart';
 import 'package:vms_app/features/auth/presentation/ui/screens/signin/sign_in_screen.dart';
 import 'package:vms_app/features/entry/screens/main_navigation.dart';
 import 'package:vms_app/features/entry/screens/splash_screen.dart';
@@ -7,9 +8,11 @@ import 'package:vms_app/features/history/presentation/ui/screens/history_screen.
 import 'package:vms_app/features/job/presentation/ui/screens/job_detail_screen.dart';
 import 'package:vms_app/features/job/presentation/ui/screens/my_jobs_screen.dart';
 import 'package:vms_app/features/location/ui/screens/location_screen.dart';
+import 'package:vms_app/features/notification/presentation/ui/screens/notification_screen.dart';
+import 'package:vms_app/features/profile/presentation/ui/screens/profile_screen.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: '/location',
+  initialLocation: '/splash',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const MainNavigation()),
     GoRoute(path: '/splash', builder: (context, state) => const SplashScreen()),
@@ -30,5 +33,14 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const HistoryScreen(),
     ),
     GoRoute(path: '/location', builder: (context, state) => LocationScreen()),
+    GoRoute(
+      path: '/problem-detail',
+      builder: (context, state) => DetailProblemScreen(),
+    ),
+    GoRoute(
+      path: '/notification',
+      builder: (context, state) => NotificationsPage(),
+    ),
+    GoRoute(path: '/profile', builder: (context, state) => ProfileScreen()),
   ],
 );
